1de4e6c61f95248a80ba11afc52a4867fab95512
kanika rungta <kanikarungta96@gmail.com>
2020-10-03 01:11:20 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -1,5 +1,4 @@
 from array import *
-print("My first Git Repository!")
 
 arr1=array('i',[5,28,9,8,7])
 arr2=array('i',[2,3,4,5,6])
@@ -9,5 +8,11 @@ leng=len(arr1)
 
 for i in range(leng):
     sum.append(arr1[i]+arr2[i])
-
+    
 print(sum)
+sum_of_array_element = 0
+for i in range(leng):
+    sum_of_array_element+=sum[i]
+    
+print("Sum of elements of the sum array: {}".format(sum_of_array_element))
+    
